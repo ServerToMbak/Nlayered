c0533272384fa4742f0350bd5bce664e6572e6fd
ServerToMbak <server78135@gmail.com>
2022-11-08 19:50:49 +0300

    day 3 GetByIdQuery, fluent Validation

--- src/demoProjects/rentACar/WebAPI/Controllers/BrandsController.cs
+++ src/demoProjects/rentACar/WebAPI/Controllers/BrandsController.cs
@@ -1,5 +1,9 @@
 ﻿using Application.Features.Brands.Commands.CreateBrand;
 using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Models;
+using Application.Features.Brands.Queries.GetById;
+using Application.Features.Brands.Queries.GetListBrand;
+using Core.Application.Requests;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,6 +18,21 @@ namespace WebAPI.Controllers
         {
             CreatedBrandDto result = await Mediator.Send(createBrandComment);
             return Created("", result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+           GetListBrandQuery getListBrandquery = new() { PageRequest = pageRequest };
+           BrandListModel result = await Mediator.Send(getListBrandquery);
+            return Ok(result);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetById([FromRoute] GetByIdBrandQuery getByIdBrandQuery)
+        {
+            BrandGetByIdDto brandGetByIdDto = await Mediator.Send(getByIdBrandQuery);
+            return Ok(brandGetByIdDto);
         }
     }
 }
