d50ddcef20b78fba617895369a2910c2c6ca67ea
ServerToMbak <server78135@gmail.com>
2022-11-14 19:37:55 +0300

    Day 5 commit

--- src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -1,5 +1,7 @@
-﻿using Domain.Entities;
+﻿using Core.Security.Entities;
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
@@ -13,8 +15,13 @@ namespace Persistence.Contexts
     {
         protected IConfiguration Configuration { get; set; }
         public DbSet<Brand> Brands { get; set; }
-        public DbSet<Model> Models { get; set; }    
-       
+        public DbSet<Model> Models { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+
 
         public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
         {
@@ -49,7 +56,53 @@ namespace Persistence.Contexts
                 a.HasOne(p=>p.brand);
 
             });
+        
 
+            modelBuilder.Entity<User>(a =>
+            {
+                a.ToTable("Users").HasKey(k => k.Id);
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.FirstName).HasColumnName("FirstName");
+                a.Property(p => p.LastName).HasColumnName("LastName");
+                a.Property(p => p.Email).HasColumnName("Email");
+                a.Property(p => p.PasswordSalt).HasColumnName("PasswordSalt");
+                a.Property(p => p.PasswordHash).HasColumnName("PasswordHash");
+                a.Property(p => p.Status).HasColumnName("Status").HasDefaultValue(true);
+                a.Property(p => p.AuthenticatorType).HasColumnName("AuthenticatorType");
+                a.HasMany(b => b.UserOperationClaims);
+                a.HasMany(b => b.RefreshTokens);
+            });
+
+            modelBuilder.Entity<OperationClaim>(a =>
+            {
+                a.ToTable("OperationClaims").HasKey(k => k.Id);
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.Name).HasColumnName("Name");
+            });
+
+            modelBuilder.Entity<RefreshToken>(a =>
+            {
+                a.ToTable("RefreshTokens").HasKey(k => k.Id);
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.UserId).HasColumnName("UserId");
+                a.Property(p => p.Token).HasColumnName("Token");
+                a.Property(p => p.Expires).HasColumnName("Expires");
+                a.Property(p => p.Created).HasColumnName("Created");
+                a.Property(p => p.CreatedByIp).HasColumnName("CreatedByIp");
+                a.Property(p => p.Revoked).HasColumnName("Revoked");
+                a.Property(p => p.RevokedByIp).HasColumnName("RevokedByIp");
+                a.Property(p => p.ReplacedByToken).HasColumnName("ReplacedByToken");
+                a.Property(p => p.ReasonRevoked).HasColumnName("ReasonRevoked");
+                a.HasOne(b => b.User);
+            });
+
+            modelBuilder.Entity<UserOperationClaim>(a =>
+            {
+                a.ToTable("UserOperationClaims").HasKey(k => k.Id);
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.OperationClaimId).HasColumnName("OperationClaimId");
+                a.Property(p => p.UserId).HasColumnName("UserId");
+            });
 
 
             Brand[] brandEntitySeeds = { new(1, "BMW"), new(2, "Mercedes") };
---
+++ src/demoProjects/rentACar/Persistence/Repositories/UserOeprationCliamRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Core.Security.Entities;
+using Persistence.Contexts;
+
+namespace Persistence.Repositories
+{
+    public class UserOperationCliamRepository : EfRepositoryBase<UserOperationClaim, BaseDbContext>, IUserOperationClaimRepository
+    {
+        public UserOperationCliamRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+
+}
